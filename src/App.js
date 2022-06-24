7681b7b4f4bdd574a2ff5097975e713cce130a06
lazy-luis <divinegiftadesiyan@gmail.com>
2022-06-24 17:51:42 +0100

    service add

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import "./App.css";
 import Contact from "./components/Contact";
 import FAQs from "./components/FAQs";
 import Portfolio from "./components/Portfolio";
+import Services from "./components/Services";
 
 const App = () => {
   return (
     <>
       <Header />
+      <Services />
       <Portfolio />
       <FAQs />
       <Contact />
---
+++ src/components/Services.js
@@ -0,0 +1,81 @@
+import React from "react";
+
+const Services = () => {
+  return (
+    <div className="componentBlock" name="ServiceSection">
+      <h3> Our Awesome Services </h3>
+      <div className="servicesBlocks">
+        <div className="serviceBlock">
+          <lottie-player
+            src={"https://assets8.lottiefiles.com/packages/lf20_yjxlc3cg.json"}
+            speed={1}
+            style={{
+              width: "250px",
+              height: "250px",
+              background: "transparent",
+            }}
+            loop
+            autoplay
+          ></lottie-player>
+          <div className="serviceInfo">
+            <h4> Web Development </h4>
+            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+          </div>
+        </div>
+        <div className="serviceBlock">
+          <lottie-player
+            src={"https://assets8.lottiefiles.com/packages/lf20_dsxct2el.json"}
+            speed={1}
+            style={{
+              width: "250px",
+              height: "250px",
+              background: "transparent",
+            }}
+            loop
+            autoplay
+          ></lottie-player>
+          <div className="serviceInfo">
+            <h4> Web Development </h4>
+            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+          </div>
+        </div>
+        <div className="serviceBlock">
+          <lottie-player
+            src={"https://assets8.lottiefiles.com/packages/lf20_w51pcehl.json"}
+            speed={1}
+            style={{
+              width: "250px",
+              height: "250px",
+              background: "transparent",
+            }}
+            loop
+            autoplay
+          ></lottie-player>
+          <div className="serviceInfo">
+            <h4> Web Development </h4>
+            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+          </div>
+        </div>
+        <div className="serviceBlock">
+          <lottie-player
+            src={"https://assets8.lottiefiles.com/packages/lf20_ov9utstn.json"}
+            speed={1}
+            style={{
+              width: "250px",
+              height: "250px",
+              background: "transparent",
+            }}
+            loop
+            autoplay
+          ></lottie-player>
+          <div className="serviceInfo">
+            <h4> Web Development </h4>
+            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Services;
