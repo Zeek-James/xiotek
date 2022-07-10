ba2e42531494dcb7d993fac78592fd4d492d3787
Lazy Luis <divinegiftadesiyan@gmail.com>
2022-07-10 13:17:49 +0100

    Update on pages

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BsInstagram, BsTwitter, BsFacebook, BsLinkedin } from "react-icons/bs";
+import { BsTwitter, BsLinkedin } from "react-icons/bs";
 
 const Team = () => {
   return (
@@ -29,10 +29,10 @@ const Team = () => {
                   <span>Co-Founder/ HR Manager</span>
                 </div>
                 <div className="social">
-                  <a href="@ccNwudo">
+                  <a href="https://t.co/ccNwudo">
                     <BsTwitter />
                   </a>
-                  <a href="@ccNwudo">
+                  <a href="https://linkedin.com/in/ccNwudo">
                     <BsLinkedin />
                   </a>
                 </div>
