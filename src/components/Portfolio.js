5edd40e3dbf7e603d5670b94b24fd0916b644fad
Lazy Luis <divinegiftadesiyan@gmail.com>
2022-07-10 21:08:14 +0100

    switches

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -4,12 +4,16 @@ const Portfolio = () => {
   return (
     <div className="componentBlock" name="PortfolioSection">
       <h3> Who We've Worked With </h3>
-      <marquee onMouseOver="this.stop()" onMouseOut="this.start()">
+      <marquee
+        direction="alternate"
+        onMouseOver="this.stop()"
+        onMouseOut="this.start()"
+      >
         <div className="portfolioBlocks">
           <div className="portfolioBlock">
             <img
-              src="http://www.nuture.tech/_nuxt/img/1ac7915.png"
-              alt="nuture tech"
+              src="https://ngfinders.com//wp-content/uploads/2019/09/Wema-Bank.jpg"
+              alt="Gogata"
               style={{
                 backgroundRepeat: "no-repeat",
                 backgroundSize: "cover",
@@ -20,6 +24,20 @@ const Portfolio = () => {
               }}
             />
           </div>
+          {/* <div className="portfolioBlock">
+            <img
+              src="https://www.nuture.tech/_nuxt/img/1ac7915.png"
+              alt="nuture tech"
+              style={{
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "cover",
+                objectFit: "contain",
+                minWidth: 200,
+                maxWidth: 400,
+                padding: 10,
+              }}
+            />
+          </div> */}
           <div className="portfolioBlock">
             <img
               src="https://media.glassdoor.com/sqll/5154/itex-squarelogo-1524658343988.png"
@@ -66,21 +84,6 @@ const Portfolio = () => {
           <div className="portfolioBlock">
             <img
               src="https://gatatransport.com.ng/wp-content/uploads/2021/07/Getatransport.png"
-              alt="Gogata"
-              style={{
-                backgroundRepeat: "no-repeat",
-                backgroundSize: "cover",
-                objectFit: "contain",
-                minWidth: 200,
-                maxWidth: 400,
-                padding: 10,
-              }}
-            />
-          </div>
-
-          <div className="portfolioBlock">
-            <img
-              src="https://ngfinders.com//wp-content/uploads/2019/09/Wema-Bank.jpg"
               alt="Gogata"
               style={{
                 backgroundRepeat: "no-repeat",
